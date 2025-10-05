8f4b8b626409ab689677eadc6f3e1d66c3f6df48
juliusmwass <juliusmwass100@gmail.com>
2025-10-05 18:06:15 +0300

    Initial commit - setup kaziHub

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+
+  return (
+    <>
+      <h1 className="p-4 font-roboto text-3xl font-bold text-blue-800">Welcome to KaziHub</h1>
+
+<p className="p-4 font-roboto text-blue-600">Manage your daily tasks effortlessly.</p>
+
+    </>
+  )
+}
+
+export default App
