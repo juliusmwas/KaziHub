099ff43821f5556d2e13345dc44d1053a694cc25
juliusmwass <juliusmwass100@gmail.com>
2025-10-08 22:13:24 +0300

    Added Search by options by set features

--- src/Components/Pages/Projects.jsx
+++ src/Components/Pages/Projects.jsx
@@ -22,10 +22,36 @@ export default function Projects () {
             </div>
 
             <div>
-                <div className="flex">
-                    <CiSearch />
-                    <input type="text" className="bg-amber-300 " />
+                <div className="mt-2 flex items-center bg-white rounded-xl px-3 py-2 w-full max-w-sm shadow-sm">
+                    <CiSearch className="text-gray-600 text-xl mr-2" />
+                    <input
+                        type="text"
+                        placeholder="Search projects here..."
+                        className="bg-transparent outline-none w-full placeholder-gray-500 text-gray-700"
+                    />
                 </div>
+
+                <div>
+                    <select name="" id="">
+                        <option value="All status">All status</option>
+                        <option value="Plannig">Planning</option>
+                        <option value="Active">Active</option>
+                        <option value="On hold">On Hold</option>
+                        <option value="Complete">Complete</option>
+                    </select>
+                </div>
+
+                <div>
+                    <select name="" id="">
+                        <option value="All Priority">All Priority</option>
+                        <option value="Plannig">High</option>
+                        <option value="Active">Medium</option>
+                        <option value="On hold">Low</option>
+                    </select>
+                </div>
+                <div>c</div>
+                <button className="bg-blue-600 text-white flex gap-4 px-4 items-center font-medium text-sm rounded-lg p-2 cursor-pointer hover:bg-blue-500"><span>+</span> Create New Project</button>
+        
             </div>
         </div>
     );
